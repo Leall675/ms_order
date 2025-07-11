24b48bcde66ecd76577baf3df50553c1f9f2cbd9
Matheus Leal Da Silva <maldsilva@rd.com.br>
2025-07-11 00:03:10 -0300

    feat: implementando método para reduzir a quantidade de produto

--- src/main/java/com/desafio/order/service/ProductsIntegrationService.java
+++ src/main/java/com/desafio/order/service/ProductsIntegrationService.java
@@ -1,5 +1,6 @@
 package com.desafio.order.service;
 
+import com.desafio.order.dto.request.StockDtoUpdate;
 import com.desafio.order.dto.response.ProductDtoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,12 +19,21 @@ public class ProductsIntegrationService {
 
     public Mono<ProductDtoResponse> getProductById(String productId) {
         return webClient.get()
-                .uri("/v1/product/{id}", productId)
+                .uri("/v1/products/{id}", productId)
                 .retrieve()
                 .bodyToMono(ProductDtoResponse.class)
                 .onErrorResume(e -> {
                     return Mono.empty();
                 });
+    }
+
+    public Mono<Void> updateProduct(String productId, long quantity) {
+        StockDtoUpdate dtoUpdate = new StockDtoUpdate(quantity, "REDUCE");
+        return webClient.patch()
+                .uri("/v1/products/{id}/stock", productId)
+                .bodyValue(dtoUpdate)
+                .retrieve()
+                .bodyToMono(Void.class);
     }
 
 }
