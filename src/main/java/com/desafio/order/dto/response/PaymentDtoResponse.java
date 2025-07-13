690f3b1443c163bcc1513b4bcfffd04678314b95
Matheus Leal Da Silva <maldsilva@rd.com.br>
2025-07-13 12:10:21 -0300

    feat: dtos de response

---
+++ src/main/java/com/desafio/order/dto/response/PaymentDtoResponse.java
@@ -0,0 +1,20 @@
+package com.desafio.order.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDtoResponse {
+    private String id;
+    private Double amount;
+    private String paymentMethod;
+    private String paymentStatus;
+    private LocalDateTime createdAt;
+}
