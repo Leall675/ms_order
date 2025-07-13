a810861ba1d2812c2acfbda84f8fb3e850892385
Matheus Leal Da Silva <maldsilva@rd.com.br>
2025-07-13 12:08:58 -0300

    feat: tratando excecoes

--- src/main/java/com/desafio/order/exception/GlobalExceptionHandler.java
+++ src/main/java/com/desafio/order/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.desafio.order.exception;
 
-import com.desafio.order.dto.error.ErroCampo;
 import com.desafio.order.dto.error.ErroResposta;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,5 +28,21 @@ public class GlobalExceptionHandler {
         resposta.setMessage(ex.getMessage());
         resposta.setErros(List.of());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErroResposta> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        ErroResposta erroResposta = new ErroResposta(HttpStatus.BAD_REQUEST.value(), "Erro de validação: verifique os tipos de dados enviados.", List.of());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erroResposta);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErroResposta> handleGenericException(Exception ex) {
+        ErroResposta resposta = new ErroResposta();
+        resposta.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        resposta.setMessage("Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.");
+        resposta.setErros(List.of());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resposta);
     }
 }
