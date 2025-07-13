ebd224d84aeaec16cc104bba5928cef0b2fba38f
Matheus Leal Da Silva <maldsilva@rd.com.br>
2025-07-13 12:10:42 -0300

    feat: método de integracao para criar pedido

--- src/main/java/com/desafio/order/service/PaymentsIntegrationService.java
+++ src/main/java/com/desafio/order/service/PaymentsIntegrationService.java
@@ -1,9 +1,14 @@
 package com.desafio.order.service;
 
+import com.desafio.order.dto.request.PaymentDtoRequest;
+import com.desafio.order.dto.response.PaymentDtoResponse;
 import com.desafio.order.dto.response.ProductDtoResponse;
+import com.desafio.order.enuns.PaymentMethod;
+import com.desafio.order.enuns.PaymentStatus;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PaymentsIntegrationService {
@@ -12,6 +17,23 @@ public class PaymentsIntegrationService {
 
     public PaymentsIntegrationService(WebClient webClientPayments) {
         this.webClient = webClientPayments;
+    }
+
+    public Mono<PaymentDtoResponse> createdPayment(Double amount, PaymentMethod paymentMethod, PaymentStatus paymentStatus) {
+        PaymentDtoRequest dtoPayment = new PaymentDtoRequest(amount, paymentMethod, paymentStatus);
+
+        return webClient.post()
+                .uri("/v1/payments")
+                .bodyValue(dtoPayment)
+                .retrieve()
+                .bodyToMono(PaymentDtoResponse.class)
+                .onErrorResume(error -> {
+                    System.err.println("Erro ao criar pagamento:" + error.getMessage());
+                    System.out.println("DTO enviado -> amount: " + dtoPayment.getAmount()
+                            + ", method: " + dtoPayment.getPaymentMethod()
+                            + ", status: " + dtoPayment.getPaymentStatus());
+                    return Mono.empty();
+                });
     }
 
 }
